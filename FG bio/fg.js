a3bbe19a2c8f36f6c5587f30964599507b00b4b5
Mahamat007 <mahamatbabol2005@gmail.com>
2024-07-19 15:44:40 +0100

    Delete FG bio directory

--- FG bio/fg.js
+++
@@ -1,7 +0,0 @@
-function scrollToTitle(titleId) {
-    const element = document.getElementById(titleId);
-    if (element) {
-      // Smooth scroll to the element with the specified ID
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-}
